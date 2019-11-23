6d19395c2320171fec616c1901c525b96d8caa10
MIHIR VAHANWALA <mjv1528@gmail.com>
2019-11-23 22:57:54 +0530

    login page works

--- VariablesSurvey/accounts/views.py
+++ VariablesSurvey/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.models import auth
+from django.contrib.auth.models import auth, User
 
 # Create your views here.
 
@@ -11,7 +11,7 @@ def login(request):
         user = auth.authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect("/")
+            return render(request, 'start.html')
         else:
             messages.info(request, 'Invalid credentials')
             return redirect('login')
@@ -21,7 +21,32 @@ def login(request):
 
 
 def register(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        first_name = request.POST['Firstname']
+        last_name = request.POST['Lastname']
+        username = request.POST['username']
+        password = request.POST['password']
+        verify = request.POST['verify']
+        email = request.POST['Email']
+        if password == verify:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'username taken')
+                return render(request, 'register.html')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'email taken')
+                return render(request, 'register.html')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                return redirect('home')
+        else:
+            messages.info(request, 'make sure you verify your password!')
+            return render(request, 'register.html')
+
+    else:
+        return render(request, 'register.html')
 
 def home(request):
     return render(request, 'index.html')
+
+
