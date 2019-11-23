304f9399d7cb21abf936d010ee767210207f1849
MIHIR VAHANWALA <mjv1528@gmail.com>
2019-11-23 21:36:03 +0530

    update

--- VariablesSurvey/accounts/views.py
+++ VariablesSurvey/accounts/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import auth
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
 
 def register(request):
-    return render(request, 'home.html')
+    return render(request, 'register.html')
