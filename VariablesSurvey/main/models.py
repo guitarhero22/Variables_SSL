447d77f2c7708731feb04d5e1704238a4dcabd29
MIHIR VAHANWALA <mjv1528@gmail.com>
2019-11-24 14:06:56 +0530

    in progress...

--- VariablesSurvey/main/models.py
+++ VariablesSurvey/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import auth, User
 
-# Create your models here.
+class Form(models.Model):
+    form_code = models.IntegerField(unique=True)
+    form_name = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
+    creator = models.ForeignKey(User, on_delete='CASCADE')
+
+
+    
